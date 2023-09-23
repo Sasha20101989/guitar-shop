f9e407474eeba0a3a036c9126488421b18491727
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-09-24 00:41:11 +0300

    add specification

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ bootstrap();
 //npm run build
 //npm run mock:server
 //npm run ts ./src/main.cli.ts -- --help
-//npm run ts ./src/main.cli.ts -- --import ./mocks/mock-data.tsv
+//npm run ts ./src/main.cli.ts -- --import ./mocks/test-data.tsv admin admin localhost guitar-shop secret
 //npm run ts ./src/main.cli.ts -- --generate 100 ./mocks/test-data.tsv http://localhost:3123/api
 //docker compose --file ./docker-compose.dev.yml --project-name "guitar-shop" up -d
