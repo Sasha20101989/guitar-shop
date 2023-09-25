20c51847f01d4fd85481ebdd1d7feb08a67d0690
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-09-25 09:35:41 +0300

    refactor

--- frontend/project/src/components/footer-logo/footer-logo.tsx
+++ frontend/project/src/components/footer-logo/footer-logo.tsx
@@ -1,9 +1,11 @@
+import { useGoToMain } from "../../hooks/use-go-to-main/use-go-to-main";
 import SocialLinkList from "../social-link-list/social-link-list";
 
 function FooterLogo(): JSX.Element {
+  const handleGoToMainClick = useGoToMain();
   return (
     <div className="footer__logo-wrapper">
-      <a className="footer__logo logo" href="main.html">
+      <a className="footer__logo logo" onClick={handleGoToMainClick}>
         <img className="logo__img" width="70" height="70" src="./img/svg/logo.svg" alt="Логотип" />
       </a>
       <div className="socials footer__socials">
--- frontend/project/src/components/header/header.tsx
+++ frontend/project/src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { useGoToLogin } from "../../hooks/use-go-to-login/use-go-to-login";
+import { useGoToMain } from "../../hooks/use-go-to-main/use-go-to-main.js";
 import HeaderNavigationList from "../header-navigation-list/header-navigation-list";
 
 type HeaderProps = {
@@ -7,6 +8,7 @@ type HeaderProps = {
 
 function Header({isLoginScreen}: HeaderProps): JSX.Element {
   const handleGoToLoginClick = useGoToLogin();
+  const handleGoToMainClick = useGoToMain();
   // const navigate = useNavigate();
   // const dispatch = useAppDispatch();
 
@@ -33,7 +35,7 @@ function Header({isLoginScreen}: HeaderProps): JSX.Element {
   return (
     <header className="header" id="header">
       <div className="container">
-        <div className="header__wrapper"><a className="header__logo logo" href="main.html"><img className="logo__img" width="70" height="70" src="./img/svg/logo.svg" alt="Логотип"/></a>
+        <div className="header__wrapper"><a className="header__logo logo" onClick={handleGoToMainClick}><img className="logo__img" width="70" height="70" src="./img/svg/logo.svg" alt="Логотип"/></a>
           <HeaderNavigationList/>
           <div className="header__container"><span className="header__user-name">Имя</span>
             <a className="header__link" onClick={handleGoToLoginClick} aria-label="Перейти в личный кабинет">
